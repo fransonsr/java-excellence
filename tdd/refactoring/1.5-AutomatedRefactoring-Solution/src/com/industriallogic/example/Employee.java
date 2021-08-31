6a12d6f91db9ff66517818e8100e757a31da95a3
Scott Franson <fransonsr@familysearch.org>
2021-08-31 09:17:37 -0600

    1.5-AutomatedRefactoring-Solution

---
+++ tdd/refactoring/1.5-AutomatedRefactoring-Solution/src/com/industriallogic/example/Employee.java
@@ -0,0 +1,36 @@
+// ***************************************************************************
+// Copyright (c) 2020, Industrial Logic, Inc., All Rights Reserved.
+//
+// This code is the exclusive property of Industrial Logic, Inc. It may ONLY be
+// used by students during Industrial Logic's workshops or by individuals
+// who are being coached by Industrial Logic on a project.
+//
+// This code may NOT be copied or used for any other purpose without the prior
+// written consent of Industrial Logic, Inc.
+// ****************************************************************************
+
+package com.industriallogic.example;
+
+import java.util.EnumSet;
+
+public abstract class Employee {
+	private int jobsCompleted = 0;
+	private int jobsSkipped = 0;
+
+	public int jobsCompletedCount() {
+		return jobsCompleted;
+	}
+
+	public int jobsSkippedCount() {
+		return jobsSkipped;
+	}
+
+	protected abstract EnumSet<Job> responsibilities();
+
+	public void performJob(Job job) {
+		if (responsibilities().contains(job))
+			jobsCompleted++;
+		else
+			jobsSkipped++;
+	}
+}
---
+++ tdd/refactoring/1.5-AutomatedRefactoring-Solution/src/com/industriallogic/example/Job.java
@@ -0,0 +1,16 @@
+// ***************************************************************************
+// Copyright (c) 2020, Industrial Logic, Inc., All Rights Reserved.
+//
+// This code is the exclusive property of Industrial Logic, Inc. It may ONLY be
+// used by students during Industrial Logic's workshops or by individuals
+// who are being coached by Industrial Logic on a project.
+//
+// This code may NOT be copied or used for any other purpose without the prior
+// written consent of Industrial Logic, Inc.
+// ****************************************************************************
+
+package com.industriallogic.example;
+
+public enum Job {
+	PROGRAM, TEST, MARKET, INTEGRATE, DESIGN, SELL, MANAGE
+}
